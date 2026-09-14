cc28922db5bd6ffd4b560fb2d3f0cb1529bcf643
Joseph King <joeckr@icloud.com>
2026-09-14 15:48:13 -0400

    feat: added ingress nginx

---
+++ scripts/install-ingress-nginx.sh
@@ -0,0 +1,9 @@
+kubectl create namespace ingress-nginx --dry-run=client -o yaml | kubectl apply -f -
+kubectl label namespace ingress-nginx pod-security.kubernetes.io/enforce=privileged --overwrite
+
+cd ../charts
+helm dependency build ingress-nginx
+
+helm install ingress-nginx -n ingress-nginx ingress-nginx/
+
+kubectl get pods -n ingress-nginx -o wide
